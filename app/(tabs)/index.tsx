3cbe7f4beb8021d8166e87d06eb90201afdaae01
Matheus171701 <124268977+Matheus171701@users.noreply.github.com>
2024-05-16 12:57:44 +0000

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,75 @@
+import { View, Text, Button, Image, StyleSheet, Platform, TouchableOpacity} from 'react-native';
+import {Link } from "expo-router";
+import {Camera, CameraView, useCameraPermissions} from "expo-camera";
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
+import { CameraType } from 'expo-camera/build/legacy/Camera.types';
+
+export default function HomeScreen() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not grant ed yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Precisamos de sua permissão para acessar a camera.</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Mude a Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  button:{
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  camera: {
+    flex: 1,
+    alignSelf: 'center'
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+});
